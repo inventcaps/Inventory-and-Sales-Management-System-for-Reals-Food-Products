1d387c5920a9670341c50ed3e5f6671b66eefabb
kimberlymacajilos <202280322@psu.palawan.edu.ph>
2025-11-09 13:24:50 +0800

    refactor database settings and add price fields to withdrawals

--- projectsite/realsproj/views.py
+++ projectsite/realsproj/views.py
@@ -3285,6 +3285,8 @@ class WithdrawItemView(View):
                             except Discounts.DoesNotExist:
                                 custom_value = discount_val
 
+                        # Initialize all price-related fields
+                        actual_unit_price = None
                         actual_discount_percent = None
                         actual_discount_amount = None
                         final_price_per_unit = None
