ed56c5d86a084c906afb21e5686535ea063bf7a5
ZuLu29 <markneilconde850@gmail.com>
2025-11-11 16:45:53 +0800

    updated showall

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
      'PASSWORD': 'Reals_db_123',
      'HOST': 'db.rczsumkmhoxjaycvggzt.supabase.co',
      'PORT': '5432',
-     'CONN_MAX_AGE': 600, 
+    #  'CONN_MAX_AGE': 600, 
  }
 }
 
