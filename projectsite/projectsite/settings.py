241f423a5c650d7423ea29c8c555a8f7a09f891b
kimberlymacajilos <202280322@psu.palawan.edu.ph>
2025-11-17 15:13:49 +0800

    update database configuration

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -97,9 +97,9 @@ WSGI_APPLICATION = 'projectsite.wsgi.application'
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'reals_local',
+       'NAME': 'reals_db',
        'USER': 'postgres',
-       'PASSWORD': 'root',
+       'PASSWORD': 'admin',
        'HOST': 'localhost',
        'PORT': '5432',
    }
