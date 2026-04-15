632e538513f2b750933b77e03dcd2ebd390d8657
kimberlymacajilos <202280322@psu.palawan.edu.ph>
2026-04-15 10:59:09 +0800

    Enhance packaging used in inventory view

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -129,7 +129,7 @@ import os
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'reals_local',
+       'NAME': 'ednoc_db',
        'USER': 'postgres',
        'PASSWORD': 'admin',
        'HOST': 'localhost',
